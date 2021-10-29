8770f843ffb79e2abec5b46c357873db6bc31747
Takashi Machinaga <zxcvbnmtacashi@gmail.com>
2021-10-30 01:19:12 +0900

    feat(vue): drop support vue.js
    BREAKING CHANGE: drop support vue.js

--- presets/vue-typescript-prettier.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    '../lib/base.js',
-    '../lib/vue.js',
-    '../lib/typescript.js',
-    '../lib/vue-typescript.js',
-    '../lib/prettier.js',
-  ],
-};
--- presets/vue3-typescript.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: [
-    '../lib/base.js',
-    '../lib/vue3.js',
-    '../lib/typescript.js',
-    '../lib/vue3-typescript.js',
-  ],
-};
